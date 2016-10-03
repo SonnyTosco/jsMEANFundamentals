ee224c2c362d96ff19eebe9713cc1599e26e7ee2
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-03 08:45:47 -0700

    finished interpreter

--- fundamentals1.js
+++ fundamentals1.js
@@ -2,55 +2,118 @@
 
 //Go thru each value in the array x , where array = [3,5,'Dojo','rocks','Michael','Sensei'].
 //Log each value
-var x = [3,5,'Dojo','rocks','Michael','Sensei'];
-for (var i = 0; i < x.length; i++) {
-  console.log(x[i]);
-}
+// var x = [3,5,'Dojo','rocks','Michael','Sensei'];
+// for (var i = 0; i < x.length; i++) {
+//   console.log(x[i]);
+// }
 
 //add a new value using the push method
-x.push(100);
-console.log(x)
+// x.push(100);
+// console.log(x)
 
 //add a new array ["hello", "world", "Javascript is fun"] to variable x.
-x.push(["hello", "world", "Javascript is fun"]);
-console.log(x)
+// x.push(["hello", "world", "Javascript is fun"]);
+// console.log(x)
 
 //create a for loop that sums all of the numbers between 1 and 500
-var sum = 0;
-for (var i = 0; i<501; i++) {
-  sum+=i;
-}
-console.log(sum)
+// var sum = 0;
+// for (var i = 0; i<501; i++) {
+//   sum+=i;
+// }
+// console.log(sum)
 
 //write a loop that will go thru the array [1,5,90,25,-3,0]. Find the min value then print
-var x = [1,5,90,25,-3,0];
-var min = 0;
-for (var i = 0; i<x.length-1; i++)
-  if(x[i] < min){
-    min = x[i];
-  }
-console.log(min);
+// var x = [1,5,90,25,-3,0];
+// var min = 0;
+// for (var i = 0; i<x.length-1; i++)
+//   if(x[i] < min){
+//     min = x[i];
+//   }
+// console.log(min);
 
 //write a loop that will go thru the array [1,5,90,25,-3,0], find the avg and print.
-var arr = [1,5,90,25,-3,0];
-var length = 0;
-var sum = 0;
-for(var i = 0; i<arr.length; i++){
-  sum+=arr[i];
-  length+=1;
-}
-console.log(sum/length);
+// var arr = [1,5,90,25,-3,0];
+// var length = 0;
+// var sum = 0;
+// for(var i = 0; i<arr.length; i++){
+//   sum+=arr[i];
+//   length+=1;
+// }
+// console.log(sum/length);
 
 //Write a for-in loop that will navigate through the object below (or write your own object):
-var new_ninja = {
- name: 'Jessica',
- profession: 'coder',
- favorite_language: 'JavaScript', //like that's even a question!
- dojo: 'Dallas'
+// var new_ninja = {
+//  name: 'Jessica',
+//  profession: 'coder',
+//  favorite_language: 'JavaScript', //like that's even a question!
+//  dojo: 'Dallas'
+// }
+// for (var key in new_ninja) {
+//   if(new_ninja.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(new_ninja[key]);
+//   }
+// }
+
+// To understand and write JavaScript code effectively, you need to understand how
+// the interpreter rearranges your code. For each of the following problems rewrite
+// the code to mimic how the interpreter would rearrange it before running.
+// After rearranging the code, predict the output.
+
+//Given
+// console.log(first_variable);
+// var first_variable = "Yipee I was first!";
+// function firstFunc() {
+//   first_variable = "Not anymore!!!";
+//   console.log(first_variable);
+// }
+// console.log(first_variable);
+
+//Re-arranged
+// var first_variable = "Yipee I was first!";
+// console.log(first_variable);
+// function firstFunc() {
+//   var first_variable;
+//   console.log(first_variable);
+// }
+// first_variable = "Not anymore!!!";
+// console.log(first_variable);
+
+//Given
+// var food = "Chicken";
+// function eat() {
+//   food = "half-chicken";
+//   console.log(food);
+//   var food = "gone";       // CAREFUL!
+//   console.log(food);
+// }
+// eat();
+// console.log(food);
+
+//Re-arranged
+// var food = "Chicken";
+// console.log(food);
+// function eat() {
+//   food = "half-chicken";
+//   console.log(food);
+//   var food = "gone";
+//   console.log(food);
+// }
+// eat();
+// ask Phil if var food needs to be used to redefine the variable food in the function
+//to change it to gone.
+
+//Given
+// var new_word = "NEW!";
+// function lastFunc() {
+//   new_word = "old";
+// }
+// console.log(new_word);
+
+//Re-arranged
+var new_word;
+new_word = "old";
+function lastFunc() {
+  var new_word = "NEW!";
 }
-for (var key in new_ninja) {
-  if(new_ninja.hasOwnProperty(key)) {
-    console.log(key);
-    console.log(new_ninja[key]);
-  }
-}
+console.log(new_word);
