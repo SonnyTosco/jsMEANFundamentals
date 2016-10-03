962cde154a8c1c0919f868aabb59793d663f7100
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-03 11:25:42 -0700

    finished functions assignment

--- fundamentals1.js
+++ fundamentals1.js
@@ -111,9 +111,73 @@
 // console.log(new_word);
 
 //Re-arranged
-var new_word;
-new_word = "old";
-function lastFunc() {
-  var new_word = "NEW!";
+// var new_word;
+// new_word = "old";
+// function lastFunc() {
+//   var new_word = "NEW!";
+// }
+// console.log(new_word);
+
+//Build some Functions
+//Make a function that can be used anywhere in your file and that when invoked
+// will console.log('I am running!'); give it the name runningLogger.
+// function runningLogger(){
+//   console.log('I am running!');
+// }
+// runningLogger();
+
+//Make a function that is callable, has one parameter and multiplies the value of
+// the parameter by 10 before returning the result. Give it the name multiplyByTen.
+// Invoke it, passing the argument 5.
+
+// function multiplyByTen(num){
+//   result = num*10;
+//   return result;
+// }
+// console.log(multiplyByTen(5));
+
+//Write two functions (stringReturnOne and stringReturnTwo) that each return
+// a different hard-coded string.
+// function stringReturnOne(){
+//   return 'Haaaa';
+// }
+// function stringReturnTwo(){
+//   return 'Huh';
+// }
+// console.log(stringReturnOne());
+// console.log(stringReturnTwo());
+
+// Write a function named caller that has one parameter. If the argument provided
+// to caller is a function (typeof may be useful), invoke the argument. Nothing is returned.
+
+// function caller(param){
+//   if(typeof(param) == 'function'){
+//     param();
+//   }
+// }
+
+//Write a function named myDoubleConsoleLog that has two parameters, if the arguments passed
+// to the function are functions, console.log the value that each, when invoked returns.
+function myDoubleConsoleLog(param1, param2){
+  if(typeof(param1) == 'function' && typeof(param2) == 'function'){
+    console.log(param1(), param2());
+  }
 }
-console.log(new_word);
+myDoubleConsoleLog(stringReturnOne, stringReturnTwo);
+
+//Write a function caller2 that has one parameter. It console.log's the string 'starting',
+// waits for 2 seconds, and then invokes the argument if the argument is a function.
+// (setTimeout may be useful for this one). The function should then console.log 'ending?'
+// and return 'Interesting.' Invoke this function by passing it myDoubleConsoleLog.
+
+function caller2(param1){
+  console.log('starting')
+  var x = setTimeout(function(){
+    if(typeof(param1) == 'function'){
+      param1(stringReturnOne, stringReturnTwo);
+    }
+  }, 2000);
+  console.log('ending?');
+  return ('interesting');
+}
+caller2(myDoubleConsoleLog);
