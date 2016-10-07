a310e30581b053a6cb4b910550473fb379770124
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-07 13:32:54 -0700

    midway thru callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,14 +1,48 @@
-function myOriginalFunction(){
-  console.log('hello');
+// function myOriginalFunction(){
+//   console.log('hello');
+// }
+//
+// // myOriginalFunction();
+//
+// function invokedFunction(callback){
+//   var x = setInterval(function(){
+//     callback();
+//   }, 4000)
+// }
+//
+// invokedFunction(myOriginalFunction);
+// invokedFunction(function(){console.log('world')});
+
+// console.log("NOW: ");
+// console.log("Declaring and assigning variable 'ninja'.");
+// var ninja = 'Libby';
+//
+// setTimeout( function myCallbackFunction(){
+//   console.log("LATER: ")
+//   console.log(ninja, "LATER"); }, 2000
+// );
+//
+// console.log("Printing ninja value.");
+// console.log(ninja, "NOW");
+
+var a = 2;
+var b = function() { console.log("something"); };
+
+function doSomething(x) {
+  console.log(typeof(x));
 }
 
-// myOriginalFunction();
+doSomething(a);
+doSomething(b);
 
-function invokedFunction(callback){
-  var x = setInterval(function(){
-    callback();
-  }, 4000)
+function doSomething(possibleCallback) {
+  if (typeof(possibleCallback) === 'function'){
+    console.log('possibleCallback is a callback!');
+    possibleCallback(); //we can invoke the callback!
+  }
+  else {
+    console.log('possibleCallback: ', possibleCallback, ' is not a callback function.');
+  }
 }
-
-invokedFunction(myOriginalFunction);
-invokedFunction(function(){console.log('world')});
+doSomething(function myCallback(){ console.log('yes, I am a callback!') });
+doSomething('string');
